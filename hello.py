37f468c873c267f52c0e03be3fcda25efbaca727
Casualty97 <ttanjunkai@gmail.com>
2026-09-04 17:10:28 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -5,12 +5,16 @@ print("Welcome here")
 print("My first post!")
 print("===================================")
 
+# a. Does the program display messages exactly as written? 
+# Yes
 
-# a. Does the program display messages exactly as written? Yes
-# b. Order of execution - top to bottom, or bottom to top? Top to bottom
-# c. Where does the output appear? in Terminal
+# b. Order of execution - top to bottom, or bottom to top? 
+# Top to bottom
 
+# c. Where does the output appear? 
+# in Terminal
 
+# Activity 2
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -19,5 +23,30 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
-# a. What is the use of the variables "username", "bio" and "followers"? They store assigned values
-# b. If you change these values, does the output change? Yes, the output will change to reflect the new values
+# a. What is the use of the variables "username", "bio" and "followers"? 
+# They store assigned values
+
+# b. If you change these values, does the output change? 
+# Yes, the output will change to reflect the new values
+
+# Activity 3
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+# a. Do we manually have to reassign the follower value? 
+# No
+
+# b. Does each operation affect the first value (100) or the existing
+#     value of followers? 
+# It affects the existing value of followers
+
+# c. What is the use of the += and -= operators? 
+# The += operator adds a value to the existing variable, while the -= operator subtracts a value from the existing variable.
