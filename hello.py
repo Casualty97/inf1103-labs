4d7e0f76b8e3dd219325cf5500cb06e05854ecde
Casualty97 <ttanjunkai@gmail.com>
2026-09-04 17:08:27 +0800

    Added Profile Variables

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+
+# Activity 1
 print("===================================")
 print("Welcome here")
 print("My first post!")
@@ -7,3 +9,15 @@ print("===================================")
 # a. Does the program display messages exactly as written? Yes
 # b. Order of execution - top to bottom, or bottom to top? Top to bottom
 # c. Where does the output appear? in Terminal
+
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+# a. What is the use of the variables "username", "bio" and "followers"? They store assigned values
+# b. If you change these values, does the output change? Yes, the output will change to reflect the new values
