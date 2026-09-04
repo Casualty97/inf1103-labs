87dcf19111aaf1c9884b4bc7a6943cd444cb9800
Casualty97 <ttanjunkai@gmail.com>
2026-09-04 17:05:58 +0800

    First Post

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("===================================")
+print("Welcome here")
+print("My first post!")
+print("===================================")
+
+
+# a. Does the program display messages exactly as written? Yes
+# b. Order of execution - top to bottom, or bottom to top? Top to bottom
+# c. Where does the output appear? in Terminal
